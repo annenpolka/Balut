6969f3ea49f6689946d1922a9e3cf9a32ea1e873
annnenpolka <k.miyakewiss1@gmail.com>
2020-08-07 13:28:02 +0900

    add axios and ElementUI

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -1,9 +1,14 @@
+import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/ja';
+import 'element-ui/lib/theme-chalk/index.css';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+Vue.use(ElementUI, { locale });
 
 new Vue({
   router,
