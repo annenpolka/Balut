f1b2721a1f561bdca2efa685733bc46c73aa0f3d
annnenpolka <k.miyakewiss1@gmail.com>
2020-08-22 11:15:19 +0900

    Vuetifyインストール

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -5,6 +5,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
@@ -13,5 +14,6 @@ Vue.use(ElementUI, { locale });
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount('#app');
