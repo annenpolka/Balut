7f82635bae25c40a0d9138d06a67edef2921c7fb
annnenpolka <k.miyakewiss1@gmail.com>
2020-08-22 18:47:58 +0900

    BookSection仮置き（BookCardテスト用）

---
+++ webapp/src/components/BookSection.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="book-section">
+    <ul>
+      <li>BookTitle</li>
+    </ul>
+    <BookCard></BookCard>
+    <BookCard></BookCard>
+    <BookCard></BookCard>
+  </div>
+</template>
+
+<script>
+import BookCard from './BookCard';
+export default {
+  components: {
+    BookCard,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.book-section {
+  @apply bg-gray-400 box-content w-3/12 max-w-sm p-2 rounded;
+}
+</style>
