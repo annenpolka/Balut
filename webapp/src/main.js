f9f723f4c7b81a5708cb9518e55e08822771ef87
annnenpolka <k.miyakewiss1@gmail.com>
2020-08-22 11:16:30 +0900

    remove elementUI

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -1,15 +1,10 @@
-import ElementUI from 'element-ui';
-import locale from 'element-ui/lib/locale/lang/ja';
-import 'element-ui/lib/theme-chalk/index.css';
 import Vue from 'vue';
 import App from './App.vue';
+import vuetify from './plugins/vuetify';
 import router from './router';
 import store from './store';
-import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
-
-Vue.use(ElementUI, { locale });
 
 new Vue({
   router,
